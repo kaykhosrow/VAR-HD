2a928fae081fddf78400359849d6f5629536fbee
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2024-05-08 00:16:54 +0100

    Update var.hd.R

--- var.hd.R
+++ var.hd.R
@@ -1,22 +1,22 @@
-var.historical.decomposition <- function(Estimation){
+var.historical.decomposition <- function(x){
   
   ## Retrieve and initialize variables 
   
-  invA    <- t(chol(as.matrix(summary(Estimation)$covres)))   # inverse of the A matrix
-  Fcomp   <- companionmatrix(Estimation)                      # Companion matrix
-  eps     <- ginv(invA) %*% t(residuals(Estimation))          # structural errors 
-  nvar    <- Estimation$K                                     # number of endogenous variables
-  nvarXeq <- nvar * Estimation$p                              # number of lagged endogenous per equation
-  nvar_ex <- 0                                                # number of exogenous (excluding constant and trend)
-  Y       <- VARmakexy(Estimation$y,Estimation$p,1)$Y         # Left-hand side
-  nobs    <- nrow(Y)                                          # number of observations
+  invA    <- t(chol(as.matrix(summary(x)$covres)))   # Inverse of the A matrix
+  Fcomp   <- companionmatrix(x)                      # Companion matrix
+  eps     <- ginv(invA) %*% t(residuals(x))          # Structural errors 
+  nvar    <- x$K                                     # Number of endogenous variables
+  nvarXeq <- nvar * x$p                              # Number of lagged endogenous per equation
+  nvar_ex <- 0                                       # Number of exogenous (excluding constant and trend)
+  Y       <- VARmakexy(x$y,x$p,1)$Y                  # Left-hand side
+  nobs    <- nrow(Y)                                 # Number of observations
   
   ## Compute historical decompositions
   
   invA_big <- matrix(0,nvarXeq,nvar)
   invA_big[1:nvar,] <- invA
-  Icomp <- cbind(diag(nvar), matrix(0,nvar,(Estimation$p-1)*nvar))
-  HDshock_big <- array(0, dim=c(Estimation$p*nvar,nobs+1,nvar))
+  Icomp <- cbind(diag(nvar), matrix(0,nvar,(x$p-1)*nvar))
+  HDshock_big <- array(0, dim=c(x$p*nvar,nobs+1,nvar))
   HDshock <- array(0, dim=c(nvar,(nobs+1),nvar))
   
   for (j in 1:nvar){  
@@ -28,13 +28,15 @@ var.historical.decomposition <- function(Estimation){
     } 
   } 
   
-  HD.shock <- array(0, dim=c((nobs+Estimation$p),nvar,nvar)) 
+  HD.shock <- array(0, dim=c((nobs+x$p),nvar,nvar)) 
   
   for (i in 1:nvar){
     for (j in 1:nvar){
-      HD.shock[,j,i] <- c(rep(NA,Estimation$p), HDshock[i,(2:dim(HDshock)[2]),j])
+      HD.shock[,j,i] <- c(rep(NA,x$p), HDshock[i,(2:dim(HDshock)[2]),j])
     }
   }
   
   return(HD.shock)
 }
+
+# To use, define the vars() object as x
